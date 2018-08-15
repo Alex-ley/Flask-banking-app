8636423e11bc76af7eb943a55b79a01a5e48a740
Alex Ley <a.p.ley@outlook.com>
2018-08-15 16:42:01 +0200

    bug fixing my_accounts

--- app.py
+++ app.py
@@ -160,12 +160,8 @@ def my_account():
     deposit_form = DepositForm()
     transfer_form = TransferForm()
     user = session['username']
-    if user:
-        account = Account.query.filter_by(name=user).first()
-        transactions = Transaction.query.filter_by(account_id=account.id).order_by(Transaction.date.desc())
-    else:
-        account = None
-        transactions = None
+    account = Account.query.filter_by(name=user).first() if user else None
+    transactions = Transaction.query.filter_by(account_id=account.id).order_by(Transaction.date.desc()) if user else None
 
     if deposit_form.deposit.data and deposit_form.validate():
         id = account.id
